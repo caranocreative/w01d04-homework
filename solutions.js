d37f8a628245fd18f4a750e0fd7ac9f398d4da5c
Laurie Carano <lauriecarano@gmail.com>
2018-06-14 21:26:16 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,10 @@
+// console.log("javascript is running");
+
+
+// Commit 1 - Verbal questions
+// What is the difference between a parameter and an argument? 
+// 		parameter is a variable listed as a part of the function
+// 		arguments are vales pass to the function when invoked
+// Within a function, what is the difference between return and console.log?
+// 		console.log is for the developer and shows in the console to debug
+// 		return returns the function in the code
